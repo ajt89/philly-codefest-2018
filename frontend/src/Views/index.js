1aba9d017a62669acc92f5a83df62ab87e6f617c
Keefer Bibby <keefer.g.bibby@gmail.com>
2018-04-14 20:27:23 -0400

    create event form

--- frontend/src/Views/index.js
+++ frontend/src/Views/index.js
@@ -1,3 +1,4 @@
 export {default as Home} from './Home/Home.js';
 export {default as Account} from './Account/Account.js';
 export {default as Login} from './Login/Login.js';
+export {default as CreateEvent} from './Event/Create/Create.js'
